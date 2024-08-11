ffe5e2d2eaa8f39389b7eccd1655d9c4da1338b9
dcschmid <daniel.schmid1980@icloud.com>
2024-08-11 13:54:46 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import playformCompress from "@playform/compress";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [icon(), playformCompress()]
+  integrations: [icon(), playformCompress()],
+  adapter: netlify()
 });
